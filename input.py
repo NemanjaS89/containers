3e8ae687d9d7dea1a94296728212b161ee888af8
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-03 18:10:36 +0200

    imported beautifulsoup functionality

--- input.py
+++ input.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import xlrd
 import xlsxwriter
+from bs4 import BeautifulSoup
 
 workbook = xlsxwriter.Workbook('containers_generated.xlsx')
 worksheet = workbook.add_worksheet()
@@ -14,7 +15,7 @@ sheet = wb.sheet_by_index(0)
 sheet.cell_value(0, 0)
         
 
-browser = webdriver.Chrome("\\Users\simic\Downloads\chromedriver")
+browser = webdriver.Chrome("resources/chromedriver.exe")
 browser.get("http://www.ictsi.hr/index.php/en/containers-tracking?fbclid=IwAR3S7tlXrquu090GqzSkA6qKPD86IBi6ThgW84FTwK2lAn-MJwiq9QPXoL4")
 
 
@@ -28,11 +29,16 @@ for i in range(sheet.nrows):
     submit_button = browser.find_element_by_class_name("readon")
     submit_button.click()
     
+    soup = BeautifulSoup(browser.page_source, 'lxml')
+    status = soup.find_all('td')[4]
+    date = soup.find_all('td')[23]
+    
     worksheet.write(row, col, sheet.cell_value(i, 0))
-    worksheet.write(row, col + 1, 'dedo')
-    worksheet.write(row, col + 2, 'bruh')
-    worksheet.write(row, col + 3, 'pomalo')
+    worksheet.write(row, col + 1, status.text)
+    worksheet.write(row, col + 2, date.text)
     row += 1
-        
+    
+    print(status.text)
+    print(date.text)
 
 workbook.close()
